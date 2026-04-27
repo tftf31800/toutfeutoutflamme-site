c19228e837c59b0900dad06d218b9095f258d3d5
tftf31800 <tftf31800@gmail.com>
2026-04-27 16:08:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import {
 } from "lucide-react";
 import { motion } from "framer-motion";
 import ArticlePrintemps from "./components/ArticlePrintemps";
+import fonctionnementpoelegranules from "./fonctionnementpoelegranules";
 
 const SITE_URL = "https://toutfeutoutflamme.eu";
 const RDV_URL = "https://tout-feu-tout-flamme-2.gazoleen.com/rdv";
