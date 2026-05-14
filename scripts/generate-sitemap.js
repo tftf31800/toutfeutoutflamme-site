a3c4fca1cc8aa3b1bd343a0f293c811a10f84286
tftf31800 <tftf31800@gmail.com>
2026-05-14 08:54:26 +0200

    Add automatic sitemap generation for SEO

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,55 @@
+import fs from "fs";
+import { SitemapStream, streamToPromise } from "sitemap";
+
+import { blogPosts } from "../src/data/blogPosts.js";
+import { seoCities } from "../src/data/cities.js";
+
+const sitemap = new SitemapStream({
+  hostname: "https://toutfeutoutflamme31.fr",
+});
+
+const staticRoutes = [
+  "/",
+  "/tarifs",
+  "/contact",
+  "/blog",
+  "/zones-intervention",
+  "/galerie",
+  "/avant-apres-entretien-poele-granules",
+  "/partenariat",
+  "/mentions-legales",
+  "/cgv",
+  "/politique-confidentialite",
+  "/cookies",
+];
+
+staticRoutes.forEach((route) => {
+  sitemap.write({
+    url: route,
+    changefreq: "weekly",
+    priority: 0.9,
+  });
+});
+
+blogPosts.forEach((post) => {
+  sitemap.write({
+    url: `/blog/${post.slug}`,
+    changefreq: "monthly",
+    priority: 0.8,
+  });
+});
+
+seoCities.forEach((city) => {
+  sitemap.write({
+    url: `/entretien-poele-granules/${city.slug}`,
+    changefreq: "monthly",
+    priority: 0.8,
+  });
+});
+
+sitemap.end();
+
+streamToPromise(sitemap).then((data) => {
+  fs.writeFileSync("./public/sitemap.xml", data.toString());
+  console.log("✅ Sitemap généré");
+});
